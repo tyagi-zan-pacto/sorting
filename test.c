4e01d75bde16ff52604b324e20991e25155827e4
Akash Bajpai <43008120+KIRAkash@users.noreply.github.com>
2019-10-03 21:58:46 +0530

    Update test.c

--- test.c
+++ test.c
@@ -2,6 +2,7 @@
 #include<stdlib.h>
 int  main()
 {
+ float j=11;
 int i=10,k=0;
 int *a;
 for(int i=0;i<=20;i++){
@@ -9,6 +10,7 @@ i++;
 k++;
 }
 a=(int*)malloc(sizeof(int)*5);
+ //dynamic memory allocation
 for(k=0;k<5;k++)
  a[k]=k;
 for(k=0;k<5;k++)
